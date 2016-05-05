6a0780e91f41f6a3570b6967648af8270d0285bb
jsonwong <wrd666520@163.com>
2016-05-05 08:43:19 +0800

    init

---
+++ i18n/zh-CN/dict.js
@@ -0,0 +1,49 @@
+﻿module.exports = {
+    experience_type: {
+        "internship": "",
+	"work":""
+    },
+    skill_type: {
+        "frontend": "Android",
+        "backend": "Web",
+        "miscellaneous": "其他"
+    },
+    education_type: {
+        "undergraduate": "（本科）"
+    },
+    link_type: {
+        "zhihu": "知乎",
+        "doubanbook": "豆瓣读书",
+        "repo": "源代码",
+        "pdf": "PDF 下载"
+    },
+    section_names: {
+        "education": {
+            "ch": "教育经历",
+            "en": "Education"
+        },
+        "language": {
+            "ch": "语言水平",
+            "en": "Language"
+        },
+        "experience": {
+            "ch": "工作经验与技能",
+            "en": "Experience and Skills"
+        },
+        "skills": {
+            "ch": "项目经验",
+            "en": "Projects"
+        }
+    },
+    lt_ie8_comment: "你正在使用 IE8 或更老的古董浏览器，还是快升级或者换浏览器吧~",
+    dict: {
+        source_code: "源代码",
+        demo: "Demo"
+    },
+    project_type: {
+        "personal": "个人项目",
+	"team":"团队项目",
+	"skills_package":"技能包",
+	"skills_package_other":"其他技能包"
+    }
+};
